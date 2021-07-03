08c6c22fd4491de1eb62977da74cf36b2133b30a
Vladimir Mijic <vlada.work.and.business@gmail.com>
2021-07-04 01:05:27 +0200

    update #2

--- itas_cms/settings.py
+++ itas_cms/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-solq06y(+z3-_11gid_niam-h_%10l3qudv=!=w^#t3naf=vf%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['digitalk.herokuapp.com', '127.0.0.1:8000', '.herokuapp.com']
 
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
